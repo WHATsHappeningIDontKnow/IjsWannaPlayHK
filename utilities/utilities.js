6d72a60410992862dfc3f8a03d285e38590f78d3
Celestrium Online <248363471+celestrium-games@users.noreply.github.com>
2026-02-04 09:39:47 -0800

    reimport site

---
+++ utilities/utilities.js
@@ -0,0 +1,5 @@
+const utilities = [
+    {name:"About:Blank Tab Cloaker", url:"./utilities/about-blank-cloaker/"},
+    {name:"Ruffle SWF Player", url:"./utilities/ruffle/"},
+    {name:"Silk (Spiral Art)", url:"./utilities/silk/"}
+];
