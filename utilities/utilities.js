1a31d4c4ccabdb937b41625a66b391da41a4adfd
Celestrium Online <248363471+celestrium-games@users.noreply.github.com>
2026-02-10 18:26:57 -0800

    add ti84 calculator

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,5 +1,6 @@
 const utilities = [
     {name:"About:Blank Tab Cloaker", url:"./utilities/about-blank-cloaker/"},
     {name:"Ruffle SWF Player", url:"./utilities/ruffle/"},
-    {name:"Silk (Spiral Art)", url:"./utilities/silk/"}
+    {name:"Silk (Spiral Art)", url:"./utilities/silk/"},
+    {name:"TI 84 Plus Emulator", url:"./utilities/ti84-calculator/"}
 ];
