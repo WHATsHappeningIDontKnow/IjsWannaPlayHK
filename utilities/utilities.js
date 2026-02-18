5919ec226bfc9e59551d31075c97d47c2d5097e6
Celestrium Online <248363471+celestrium-games@users.noreply.github.com>
2026-02-18 15:05:36 -0800

    remove gradevue

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,6 +1,6 @@
 const utilities = [
     {name:"About:Blank Tab Cloaker", url:"./utilities/about-blank-cloaker/"},
-    {name:"Grade Compass", url:"./utilities/gradecompass/"},
+    //{name:"Grade Compass", url:"./utilities/gradecompass/"},
     {name:"Ruffle SWF Player", url:"./utilities/ruffle/"},
     {name:"Silk (Spiral Art)", url:"./utilities/silk/"},
     {name:"TI 84 Plus Emulator", url:"./utilities/ti84-calculator/"}
